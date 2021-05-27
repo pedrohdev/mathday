7000c2885eea469cba5ab35d5cb766ace9074ef4
Pedro Henrique Vieira <64736691+PedroHdev@users.noreply.github.com>
2021-05-27 13:41:35 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -17,6 +17,11 @@ export default [
         answer: 3
     },
     {
+        question: "A matemática é uma ciência?",
+        choices: ["Não, pois não relaciona as práticas do cotidiano e a natureza ao raciocínio humano e à lógica numérica", "A matemática é a ciência do raciocínio lógico e abstrato", "A Matemática não é uma ciência, pois não relaciona a lógica com situações práticas habituais", "Todas as alternativas acima estão erradas"],
+        answer: 1
+    },
+    {
         question: "A temperatura na cidade X é de 20°C <br> e o da cidade Y é sempre a temperatura da cidade X + X/2. <br> Qual a temperatura na cidade Y?",
         choices: ["31°C", "30°C", "28°C", "25°C"],
         answer: 1
